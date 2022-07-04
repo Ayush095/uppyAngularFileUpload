3de7a6ba466863676a6d71a5bd142d72cc8d3213
Ayush <ayushbansal095@gmail.com>
2022-07-04 12:40:12 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UppyAngularDashboardModule } from '@uppy/angular';
+import {UppyAngularDragDropModule } from '@uppy/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UppyAngularDashboardModule,
+    UppyAngularDragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
